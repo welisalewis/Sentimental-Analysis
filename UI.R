b633c471323d0ddaf8d2b3c340f04a6b1ef971a1
welisalewis <welisalewis1@gmail.com>
2018-03-18 13:07:03 +0530

    change

--- UI.R
+++ UI.R
@@ -3,7 +3,7 @@ shinyUI(fluidPage(
   titlePanel("Movie-Review-Analysis"),
   div(
     id = "form",
-    textInput("name","Name of movie",""),
+    textInput("name","Name of movieeee",""),
     #actionButton("submit","Submit",class="btn-primary"),
     sliderInput("maxTweets","Number of recent tweets to use for analysis:",min=100,max=1000,value=450,step=1), # The max can, of course, be increased
     actionButton("sub",icon =icon("twitter"), label="Get Tweets!")
